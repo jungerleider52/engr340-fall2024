76214ab133025b0fce84b157b0a579dc47095f58
jungerleider52 <ungerljs@dukes.jmu.edu>
2024-11-03 22:20:29 -0500

    to date

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -8,11 +8,16 @@ path = '../../../data/ekg/mitdb_201.csv'
 # load data in matrix from CSV file; skip first two rows
 
 ### Your code here ###
-
+data = np.loadtxt(path, delimiter=',', skiprows=2)
 # save each vector as own variable
 
 ### Your code here ###
-
+time = data[:,0]
+v5 = data[:,1]
+v2 = data[:,2]
 # use matplot lib to generate a single
 
 ### Your code here ###
+plt.plot(time, v5)
+plt.show()
+
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as sp
 
 """
 Step 0: Select which database you wish to use.
@@ -18,33 +19,44 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
-## YOUR CODE HERE ##
+signal = np.loadtxt(signal_filepath, delimiter=',', skiprows=2)
+#time = signal[:,0]
+signal = signal[:,1]
+#v2 = signal[:,2]
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
 """
+#data = sp.butter(data, fs=250, N=6)
 
-## YOUR CODE HERE ##
 
 """
 Step 3: Pass data through weighted differentiator
 """
-
-## YOUR CODE HERE ##
-
-
+signal_diff = np.diff(signal)
 """
 Step 4: Square the results of the previous step
 """
- ## YOUR CODE HERE ##
-
+signal_square = np.square(signal_diff)
 """
 Step 5: Pass a moving filter over your data
 """
+signal_convolve = np.convolve(signal_square, [1,1,1])
 
-## YOUR CODE HERE
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+fig, axs = plt.subplots(2,2)
+
+axs[0, 0].set_title('Raw V5 Signal')
+axs[0, 0].plot(signal)
+
+axs[0, 1].set_title('V5 After Differentiation')
+axs[0, 1].plot(signal_diff)
+
+axs[1, 0].set_title('V5 After Squaring')
+axs[1, 0].plot(signal_square)
+
+axs[1, 1].set_title('V5 After Convolution')
+axs[1, 1].plot(signal_convolve)
+
+plt.tight_layout()
 plt.show()
